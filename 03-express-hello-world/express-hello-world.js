26d783d1173bed6c3dc48ecedcaf7034f9485001
Ben Conner <ben.conner40@gmail.com>
2017-03-23 15:59:28 -0500

    built an node express helloworld app with port selection

---
+++ 03-express-hello-world/express-hello-world.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+console.log(express);
+
+app.set('port', process.env.PORT || 8080);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/time', (req, res)=>{
+  let isodate = new Date().toISOString()
+  res.send(isodate)
+})
+
+app.listen(app.get('port'));
